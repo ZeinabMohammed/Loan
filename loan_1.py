f21fa9022b026787c585af019b285a653465adce
zeinabmohammed <zobalhlooba@gmail.com>
2019-08-12 22:40:54 +0200

    add db to same file

--- loan_1.py
+++ loan_1.py
@@ -1,5 +1,18 @@
-#Import DB:
-from models import Loan, InvestorLoanOffer
+# Create Data base:
+
+class Loan(object):
+    """loan table with amount and period attributes"""
+    def __init__(self, Amount,period):
+        self.Amount = Amount
+        self.period =period
+        self.status='requested'
+
+class InvestorLoanOffer(object):
+    """loan investor offer table with investor balance and annual interest rate attributes"""
+    def __init__(self, investor_balance, annual_interest_rate):
+        self.investor_balance = investor_balance
+        self.annual_interest_rate=annual_interest_rate
+        
 
 
 #use Case initial values:
